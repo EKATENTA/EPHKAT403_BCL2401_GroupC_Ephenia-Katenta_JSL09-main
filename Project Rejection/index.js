5ef7ec693aa9e7a8a2a379183fcc278413fec14d
Ephenia Katenta <katentaephenia@gmail.com>
2024-04-12 13:25:42 +0200

    added Author info

--- Project Rejection/index.js
+++ Project Rejection/index.js
@@ -4,6 +4,7 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
+	 document.getElementById("author").textContent = `By: ${data.user.name}`;
   });
 
 
